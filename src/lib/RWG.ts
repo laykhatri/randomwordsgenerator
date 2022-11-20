033dc813c5d7e31243f77ad760f065430e671bad
LayKhatri <laykhatri@hotmail.com>
2022-11-20 21:31:04 +0530

    V 1.3.0 - 20th Nov 2022

--- src/lib/RWG.ts
+++ src/lib/RWG.ts
@@ -1,98 +1,114 @@
-interface iRWGOptions{
-    count:NumberUndefined;
-    minLength:NumberUndefined;
-    maxLength: NumberUndefined ;
-    exactLength: NumberUndefined ;
+interface iRWGOptions {
+  count: NumberUndefined;
+  minLength: NumberUndefined;
+  maxLength: NumberUndefined;
+  exactLength: NumberUndefined;
+  mustContain: StringUndefined;
 }
 
 type NumberUndefined = number | undefined;
+type StringUndefined = string | undefined;
 
-class RWGOptions implements iRWGOptions{
-    count: NumberUndefined = 5;
-    minLength: NumberUndefined = 0;
-    maxLength: NumberUndefined = 0;
-    exactLength: NumberUndefined = 0;
+class RWGOptions implements iRWGOptions {
+  mustContain: StringUndefined = undefined;
+  count: NumberUndefined = 5;
+  minLength: NumberUndefined = 0;
+  maxLength: NumberUndefined = 0;
+  exactLength: NumberUndefined = 0;
 }
-class RWG{
+class RWG {
+  private wordList: string[] = [];
+  private wordListLength: number = 0;
 
-    private wordList: string[] = [];
-    private wordListLength: number=0;
+  constructor(words: string[] | undefined = undefined) {
+    words === undefined ? this.ReadDataFromZip() : (this.wordList = words);
+    this.InitBaseData();
+  }
 
-    constructor()
-    {
-       this.ReadDataFromZip();
-       this.InitBaseData();
+  private ReadDataFromZip() {
+    var admZip = require("adm-zip");
+    const content = new admZip(__dirname + "\\words.zip");
+    const decompressedData = content
+      .getEntries()[0]
+      .getData()
+      .toString("utf-8");
+    this.wordList = decompressedData.split(",");
+  }
+
+  private InitBaseData() {
+    this.wordListLength = this.wordList.length;
+  }
+
+  public GetWords(options: Partial<RWGOptions> = new RWGOptions()) {
+    if (options.count === undefined) {
+      options.count = 5;
     }
-
-    private ReadDataFromZip()
-    {
-        var admZip = require("adm-zip");
-        const content = new admZip(__dirname +"\\words.zip");
-        const decompressedData = content.getEntries()[0].getData().toString("utf-8");
-        this.wordList = decompressedData.split(",");
+    if (options!.exactLength! > 0) {
+      return this.GetLengthBased(
+        options!.exactLength!,
+        options!.exactLength!,
+        options.count!,
+        options!.mustContain === undefined ? "" : options.mustContain!
+      );
+    } else if (options!.maxLength! > 0 || options!.minLength! > 0) {
+      return this.GetLengthBased(
+        options!.minLength!,
+        options!.maxLength!,
+        options.count!,
+        options!.mustContain === undefined ? "" : options.mustContain!
+      );
+    } else {
+      return this.GetRandomWords(options.count!, options!.mustContain!);
     }
+  }
 
-    private InitBaseData()
-    {
-        this.wordListLength = this.wordList.length;
+  public GetWordsCount() {
+    return this.wordListLength;
+  }
+
+  private GetRandomIndex = (length: number) => {
+    return Math.round(Math.random() * length);
+  };
+
+  private GetRandomWords(count: number, mustContain: string) {
+    let returnWords: string[] = [];
+    let words: string[] = [];
+    let wordsLength: number = 0;
+    if (mustContain !== "") {
+      words = this.wordList.filter((w) => w.includes(mustContain));
+      wordsLength = words.length;
     }
-
-    public GetWords(options : Partial<RWGOptions>=new RWGOptions())
-    {
-        if(options.count === undefined || options.count < 1)
-        {
-            options.count = 5;
-        }
-        if(options!.exactLength! > 0)
-        {
-            return this.GetExactLengthWords(options!.exactLength!,options!.count!);
-        }
-        else if(options!.maxLength!>0 || options!.minLength! > 0)
-        {
-            return this.GetLengthBased(options!.minLength!,options!.maxLength!,options.count!);
-        }
-
-        return this.GetRandomWords(options.count!);
+    for (let i = 0; i < count; i++) {
+      if (wordsLength > 0) {
+        returnWords.push(words[this.GetRandomIndex(wordsLength)]);
+      } else {
+        returnWords.push(
+          this.wordList[this.GetRandomIndex(this.wordListLength)]
+        );
+      }
     }
+    return returnWords;
+  }
 
-    private GetRandomIndex = (length:number) => {
-        return Math.round( Math.random() * length);
+  private GetLengthBased(
+    min: number,
+    max: number,
+    count: number,
+    mustContain: string
+  ) {
+    let returnWords: string[] = [];
+    let lengthBasedList: string[] = this.wordList.filter(
+      (f) =>
+        (min > 0 ? f.length >= min : f.length > -1) &&
+        (max > 0 ? f.length <= max : f.length > -1) &&
+        (mustContain !== "" ? f.includes(mustContain) : f.includes(""))
+    );
+    const lengthBasedLength: number = lengthBasedList.length;
+    for (let i = 0; i < count; i++) {
+      returnWords.push(lengthBasedList[this.GetRandomIndex(lengthBasedLength)]);
     }
-
-    private GetRandomWords(count:number)
-    {
-        var returnWords: string[] = [];
-        for(let i = 0;i<count;i++)
-        {
-            returnWords.push(this.wordList[this.GetRandomIndex(this.wordListLength)])
-        }
-        return returnWords;
-    }
-
-    private GetExactLengthWords(chars:number, count:number)
-    {
-        var returnWords: string[] = [];
-        let exactLengthList = this.wordList.filter(w=> w.length == chars);
-        const exactLength = exactLengthList.length;
-        for(let i =0;i<count;i++)
-        {
-            returnWords.push(exactLengthList[this.GetRandomIndex(exactLength)]);
-        }
-        return returnWords;
-    }
-
-    private GetLengthBased(min:number, max:number, count:number)
-    {
-        let returnWords : string[] = [];
-        let lengthBasedList : string[] = this.wordList.filter( f => (min > 0 ? f.length>=min : f.length > -1) && (max > 0 ? f.length<=max : f.length > -1));
-        const lengthBasedLength : number = lengthBasedList.length;
-        for(let i =0;i<count;i++)
-        {
-            returnWords.push(lengthBasedList[this.GetRandomIndex(lengthBasedLength)]);
-        }
-        return returnWords;
-    }
-
+    return returnWords;
+  }
 }
 
-export {RWG};
+export { RWG };
--- src/tests/unit.test.ts
+++ src/tests/unit.test.ts
@@ -1,73 +1,187 @@
-import {RWG} from "../lib/RWG";
+import { RWG } from "../lib/RWG";
 
-describe("Unit Testing",()=>{
-    test("Should create object",()=>{
-        let obj = new RWG();
-        expect(obj).not.toBe(null);
-    })
-    test("Should return 5 Words",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords();
-        expect(words.length).toBe(5);
-    })
-    test("Should return 10 Words",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({count:10});
-        expect(words.length).toBe(10);
-    })
-    test("Should return 5 Words with length 5",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({exactLength:5});
-        expect(words.length).toBe(5);
-        expect(words.every(w=> w.length===5)).toBe(true);
-    })
-    test("Should return 15 Words with length 5",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({exactLength:5, count:15});
-        expect(words.length).toBe(15);
-        expect(words.every(w=> w.length===5)).toBe(true);
-    })
-    test("Should return 5 Words with Minimum 5 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({minLength:5});
-        expect(words.length).toBe(5);
-        expect(words.every(w=> w.length>=5)).toBe(true);
-    })
-    test("Should return 5 Words with Maximum 5 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({maxLength:5});
-        expect(words.length).toBe(5);
-        expect(words.every(w=> w.length<=5)).toBe(true);
-    })
-    test("Should return 15 Words with Minimum 5 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({minLength:5, count:15});
-        expect(words.length).toBe(15);
-        expect(words.every(w=> w.length>=5)).toBe(true);
-    })
-    test("Should return 15 Words with Maximum 5 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({maxLength:5, count:15});
-        expect(words.length).toBe(15);
-        expect(words.every(w=> w.length<=5)).toBe(true);
-    })
-    test("Should return 5 Words with Maximum 5 char and Minimum 3 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({maxLength:5, minLength:3});
-        expect(words.length).toBe(5);
-        expect(words.every(w=> w.length>=3 && w.length<=5)).toBe(true);
-    })
-    test("Should return 15 Words with Maximum 5 char and Minimum 3 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({maxLength:5, minLength:3,count:15});
-        expect(words.length).toBe(15);
-        expect(words.every(w=> w.length>=3 && w.length<=5)).toBe(true);
-    })
-    test("Should return 15 Words with Exact 7 chars regardless of Maximum 5 char and Minimum 3 char",()=>{
-        let rwg = new RWG();
-        let words = rwg.GetWords({maxLength:5, minLength:3, exactLength:7, count:15});
-        expect(words.length).toBe(15);
-        expect(words.every(w=> w.length===7)).toBe(true);
-    })
-    
-})
+describe("Object Tesing", () => {
+  test("Should create object", () => {
+    let obj = new RWG();
+    expect(obj).not.toBe(null);
+  });
+});
+
+describe("Words with count", () => {
+  test("Should return 0 Words even Negative count sent", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ count: -5 });
+    expect(words.length).toBe(0);
+  });
+  test("Should return 0 Words", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ count: 0 });
+    expect(words.length).toBe(0);
+  });
+  test("Should return 5 Words", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords();
+    expect(words.length).toBe(5);
+  });
+  test("Should return 10 Words", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ count: 10 });
+    expect(words.length).toBe(10);
+  });
+  test("Should return 0 Words With using mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ count: 0, mustContain: "he" });
+    expect(words.length).toBe(0);
+  });
+  test("Should return 5 Words with using mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ mustContain: "ap" });
+    expect(words.length).toBe(5);
+  });
+  test("Should return 10 Words with using mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ count: 10, mustContain: "be" });
+    expect(words.length).toBe(10);
+  });
+});
+
+describe("Words with Exact Length", () => {
+  test("Should return 5 Words with length 5", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ exactLength: 5 });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length === 5)).toBe(true);
+  });
+  test("Should return 15 Words with length 5", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ exactLength: 5, count: 15 });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length === 5)).toBe(true);
+  });
+  test("Should return 5 Words with length 5 with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ exactLength: 5, mustContain: "he" });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length === 5)).toBe(true);
+  });
+  test("Should return 15 Words with length 5 with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ exactLength: 5, count: 15, mustContain: "be" });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length === 5)).toBe(true);
+  });
+});
+
+describe("Words with Minimum Lenght", () => {
+  test("Should return 5 Words with Minimum 5 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ minLength: 5 });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length >= 5)).toBe(true);
+  });
+  test("Should return 15 Words with Minimum 5 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ minLength: 5, count: 15 });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length >= 5)).toBe(true);
+  });
+  test("Should return 5 Words with Minimum 5 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ minLength: 5, mustContain: "pe" });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length >= 5)).toBe(true);
+  });
+  test("Should return 15 Words with Minimum 5 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ minLength: 5, count: 15, mustContain: "te" });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length >= 5)).toBe(true);
+  });
+});
+
+describe("Words with Maximum Length", () => {
+  test("Should return 5 Words with Maximum 5 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5 });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length <= 5)).toBe(true);
+  });
+
+  test("Should return 15 Words with Maximum 5 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5, count: 15 });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length <= 5)).toBe(true);
+  });
+  test("Should return 5 Words with Maximum 5 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5, mustContain: "re" });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length <= 5)).toBe(true);
+  });
+
+  test("Should return 15 Words with Maximum 5 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5, count: 15, mustContain: "ge" });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length <= 5)).toBe(true);
+  });
+});
+
+describe("Words with Minimum and Maximum Length", () => {
+  test("Should return 5 Words with Maximum 5 char and Minimum 3 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5, minLength: 3 });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length >= 3 && w.length <= 5)).toBe(true);
+  });
+  test("Should return 15 Words with Maximum 5 char and Minimum 3 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 5, minLength: 3, count: 15 });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length >= 3 && w.length <= 5)).toBe(true);
+  });
+  test("Should return 5 Words with Maximum 7 char and Minimum 3 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({ maxLength: 7, minLength: 3, mustContain: "ab" });
+    expect(words.length).toBe(5);
+    expect(words.every((w) => w.length >= 3 && w.length <= 7)).toBe(true);
+  });
+  test("Should return 15 Words with Maximum 7 char and Minimum 3 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({
+      maxLength: 7,
+      minLength: 3,
+      count: 15,
+      mustContain: "mi",
+    });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length >= 3 && w.length <= 7)).toBe(true);
+  });
+});
+
+describe("Words with Extra Length Priortize other Minimum and Maximum", () => {
+  test("Should return 15 Words with Exact 7 chars regardless of Maximum 5 char and Minimum 3 char", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({
+      maxLength: 5,
+      minLength: 3,
+      exactLength: 7,
+      count: 15,
+    });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length === 7)).toBe(true);
+  });
+  test("Should return 15 Words with Exact 7 chars regardless of Maximum 5 char and Minimum 3 char with mustContain", () => {
+    let rwg = new RWG();
+    let words = rwg.GetWords({
+      maxLength: 5,
+      minLength: 3,
+      exactLength: 7,
+      count: 15,
+      mustContain: "fe",
+    });
+    expect(words.length).toBe(15);
+    expect(words.every((w) => w.length === 7)).toBe(true);
+  });
+});
